ce4c57c7890ef61100c1706a316c6d897184d5c0
mateusasilva-dev <mateuszitosilva@gmail.com>
2026-05-16 23:20:37 -0300

    feat: adiciona página de nova ordem de serviço refactor(sidebar): organiza e melhora a legibilidade feat(sidebar): adiciona animações feat(icons): adiciona dois novos ícones

--- src/js/pages/proprietario/dashboard.js
+++ src/js/pages/proprietario/dashboard.js
@@ -6,27 +6,27 @@ const atrasado = 1;
 
 const cliente = 14;
 
-function Porcentagem(valor, total){
-    return (valor/total)*100;
+function Porcentagem(valor, total) {
+    return (valor / total) * 100;
 }
 
-function Alerta(id, valor){
+function Alerta(id, valor) {
     document.getElementById("alerta").textContent =
         `Você tem ${valor} ordem atrasada(s)`;
 }
 
-Alerta("atrasado", atrasado)
+Alerta("atrasado", atrasado);
 
-function ValorIndice(id, valor, total){
-    const pct = Porcentagem(valor, total)
+function ValorIndice(id, valor, total) {
+    const pct = Porcentagem(valor, total);
 
     document.getElementById("indice-" + id).textContent = `${Math.round(pct)}%`;
 
     const alerta = document.getElementById("alerta-atrasado");
 
-    if(pct > 4){
+    if (pct > 4) {
         alerta.textContent = `Requer Atenção!`;
-    }else{
+    } else {
         alerta.textContent = ``;
     }
 }
@@ -34,15 +34,15 @@ function ValorIndice(id, valor, total){
 ValorIndice("concluido", concluido, total);
 ValorIndice("atrasado", atrasado, total);
 
-function ValorQuan(id, valor){
+function ValorQuan(id, valor) {
     document.getElementById("num-" + id).textContent = `${valor}`;
 }
 
 ValorQuan("cliente", cliente);
-ValorQuan("concluido", concluido)
+ValorQuan("concluido", concluido);
 
-function AtualizarBarra(id, valor, total){
-    const pct = Porcentagem(valor, total)
+function AtualizarBarra(id, valor, total) {
+    const pct = Porcentagem(valor, total);
 
     document.getElementById("barra-" + id).style.width = pct + "%";
 
@@ -55,7 +55,7 @@ AtualizarBarra("andamento", andamento, total);
 AtualizarBarra("pendente", pendente, total);
 AtualizarBarra("atrasado", atrasado, total);
 
-function AtualizarStatus(id, numeroOS, status){
+function AtualizarStatus(id, numeroOS, status) {
     const container = document.getElementById("status-os-" + id);
     const img = document.getElementById("img-status-" + id);
     const text = document.getElementById("status-texto-" + id);
@@ -65,14 +65,26 @@ function AtualizarStatus(id, numeroOS, status){
     container.className = "OS-status " + status;
 
     const estados = {
-        concluido: { img: "../../assets/icons/icon-green-check-circle.svg", texto: "Concluído" },
-        andamento: { img: "../../assets/icons/icon-orange-clock.svg", texto: "Em andamento" },
-        atrasado: { img: "../../assets/icons/icon-red-warning-circle.svg", texto: "Atrasado" },
-        pendente : {img: "../../assets/icons/icon-blue-file.svg", texto: "Pendente"}
+        concluido: {
+            img: "../../assets/icons/icon-green-check-circle.svg",
+            texto: "Concluído",
+        },
+        andamento: {
+            img: "../../assets/icons/icon-orange-clock.svg",
+            texto: "Em andamento",
+        },
+        atrasado: {
+            img: "../../assets/icons/icon-red-warning-circle.svg",
+            texto: "Atrasado",
+        },
+        pendente: {
+            img: "../../assets/icons/icon-blue-file.svg",
+            texto: "Pendente",
+        },
     };
 
     img.src = estados[status].img;
-    text.textContent = estados[status].texto;   
+    text.textContent = estados[status].texto;
 }
 
 AtualizarStatus(5, 5, "andamento");
